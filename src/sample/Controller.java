704f04913aea792dfd9b22ba131ffaa9ea636d8c
Kostya <KonstantinLikes@gmail.com>
2020-08-30 18:36:59 +1000

    Калькулятор и окно чата!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextArea chat;
+    @FXML
+    private TextField message;
+
+    public void sendMessage(ActionEvent actionEvent) {
+        chat.appendText(message.getText() + "\n");
+        message.clear();
+    }
 }
